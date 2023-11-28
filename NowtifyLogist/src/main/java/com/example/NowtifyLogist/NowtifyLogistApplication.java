a432424bf5f4aebae195802831e073ebd6a4b567
DiegoRomero02 <78620813+Diego20-svg@users.noreply.github.com>
2023-11-28 16:10:47 -0500

    Add files via upload

---
+++ NowtifyLogist/src/main/java/com/example/NowtifyLogist/NowtifyLogistApplication.java
@@ -0,0 +1,13 @@
+package com.example.NowtifyLogist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NowtifyLogistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NowtifyLogistApplication.class, args);
+	}
+
+}
---
+++ NowtifyLogist/src/test/java/com/example/NowtifyLogist/NowtifyLogistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.NowtifyLogist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NowtifyLogistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
